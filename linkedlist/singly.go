f557e1110d862c22f58e6ad5322554f564ea5e89
Brady DeBoer <bradydeboer195@gmail.com>
2026-03-17 11:54:18 -0400

    linked list

---
+++ linkedlist/singly.go
@@ -0,0 +1,113 @@
+package linkedlist
+
+import "errors"
+
+type Singly[T any] struct {
+	head *node[T]
+}
+
+type node[T any] struct {
+	val  T
+	next *node[T]
+}
+
+func (l *Singly[T]) Length() (i int) {
+	if l.head == nil {
+		return 0
+	}
+
+	i++
+
+	currentNode := l.head
+	for currentNode.next != nil {
+		currentNode = currentNode.next
+		i++
+	}
+
+	return i
+}
+
+func (l *Singly[T]) Insert(val T, i int) (err error) {
+	if i == 0 {
+		l.Prepend(val)
+		return
+	}
+
+	if i < 0 || l.head == nil {
+		return errors.New("index out of range")
+	}
+
+	currentNode := l.head
+	for range i - 1 {
+		if currentNode.next == nil {
+			return errors.New("index out of range")
+		}
+
+		currentNode = currentNode.next
+	}
+
+	currentNode.next = &node[T]{val: val, next: currentNode.next}
+	return nil
+}
+
+func (l *Singly[T]) Delete(i int) (err error) {
+	if l.head == nil {
+		return errors.New("index out of range")
+	}
+
+	if i == 0 {
+		l.head = l.head.next
+		return
+	}
+
+	currentNode := l.head
+	for range i - 1 {
+		if currentNode.next == nil {
+			return errors.New("index out of range")
+		}
+
+		currentNode = currentNode.next
+	}
+
+	if currentNode.next == nil {
+		return errors.New("index out of range")
+	}
+
+	currentNode.next = currentNode.next.next
+	return nil
+}
+
+func (l *Singly[T]) Append(val T) {
+	if l.head == nil {
+		l.head = &node[T]{val: val}
+		return
+	}
+
+	currentNode := l.head
+	for currentNode.next != nil {
+		currentNode = currentNode.next
+	}
+
+	currentNode.next = &node[T]{val: val}
+}
+
+func (l *Singly[T]) Prepend(val T) {
+	l.head = &node[T]{val: val, next: l.head}
+}
+
+func (l *Singly[T]) Get(i int) (val T, err error) {
+	if i < 0 || l.head == nil {
+		return val, errors.New("index out of range")
+	}
+
+	currentNode := l.head
+	for range i {
+		if currentNode.next == nil {
+			return val, errors.New("index out of range")
+		}
+
+		currentNode = currentNode.next
+	}
+
+	return currentNode.val, nil
+}
---
+++ linkedlist/singly_test.go
@@ -0,0 +1,93 @@
+package linkedlist_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/debobrad579/dsa/linkedlist"
+)
+
+func TestSinglyLength(t *testing.T) {
+	var l linkedlist.Singly[int]
+	assert.Equal(t, 0, l.Length())
+	l.Append(1)
+	assert.Equal(t, 1, l.Length())
+	l.Append(2)
+	l.Append(3)
+	assert.Equal(t, 3, l.Length())
+}
+
+func TestSinglyAppend(t *testing.T) {
+	var l linkedlist.Singly[int]
+	l.Append(1)
+	val, err := l.Get(0)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, val)
+	l.Append(2)
+	l.Append(3)
+	val, err = l.Get(2)
+	assert.NoError(t, err)
+	assert.Equal(t, 3, val)
+}
+
+func TestSinglyPrepend(t *testing.T) {
+	var l linkedlist.Singly[int]
+	l.Prepend(1)
+	val, err := l.Get(0)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, val)
+	l.Prepend(2)
+	val, err = l.Get(0)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, val)
+}
+
+func TestSinglyGet(t *testing.T) {
+	var l linkedlist.Singly[int]
+	_, err := l.Get(0)
+	assert.Error(t, err)
+	l.Append(10)
+	l.Append(20)
+	l.Append(30)
+	val, err := l.Get(0)
+	assert.NoError(t, err)
+	assert.Equal(t, 10, val)
+	val, err = l.Get(2)
+	assert.NoError(t, err)
+	assert.Equal(t, 30, val)
+	_, err = l.Get(3)
+	assert.Error(t, err)
+	_, err = l.Get(-1)
+	assert.Error(t, err)
+}
+
+func TestSinglyInsert(t *testing.T) {
+	var l linkedlist.Singly[int]
+	err := l.Insert(1, -1)
+	assert.Error(t, err)
+	err = l.Insert(1, 1)
+	assert.Error(t, err)
+	l.Append(10)
+	l.Append(30)
+	err = l.Insert(20, 1)
+	assert.NoError(t, err)
+	val, err := l.Get(1)
+	assert.NoError(t, err)
+	assert.Equal(t, 20, val)
+}
+
+func TestSinglyDelete(t *testing.T) {
+	var l linkedlist.Singly[int]
+	err := l.Delete(-1)
+	assert.Error(t, err)
+	err = l.Delete(0)
+	assert.Error(t, err)
+	l.Append(10)
+	l.Append(30)
+	err = l.Delete(1)
+	assert.NoError(t, err)
+	val, err := l.Get(0)
+	assert.NoError(t, err)
+	assert.Equal(t, 10, val)
+}
